193eb465acda5bd326ce19845033777cef8ce261
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-12-04 22:47:06 +0100

    Landing page and routing

--- ng-forms-bootstrap/src/app/app.module.ts
+++ ng-forms-bootstrap/src/app/app.module.ts
@@ -2,20 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { rootRouterConfig } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(rootRouterConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
